95d2911c334000b184d49f1e564611abc4a1b2f4
satwik-bhardwaj <satwikbhardwaj123@gmail.com>
2025-08-31 13:29:03 +0530

    enabling-cors-origin-via-environments functionality enabled

--- src/main/java/com/satwik/splitora/configuration/WebConfig.java
+++ src/main/java/com/satwik/splitora/configuration/WebConfig.java
@@ -1,16 +1,22 @@
 package com.satwik.splitora.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("#{'${allowed.origins}'.split(',')}")
+    private List<String> allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(allowedOrigins.toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
